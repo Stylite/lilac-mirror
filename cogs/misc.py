e3060061e5db9639ddb9440b122ea3b1f841fced
adx59 <adamxu59@gmail.com>
2018-01-24 17:04:57 -0800

    Help command 1/2 implemented

--- cogs/misc.py
+++ cogs/misc.py
@@ -10,17 +10,34 @@ class Misc:
     @commands.command()
     async def help(self, ctx, *args):
         send = discord.Embed()
+
+        cats = [cog for cog in self.bot.cogs]
+        cats.sort()
+        all_cmds = []
+        for cog in self.bot.cogs:
+            cmds = self.bot.get_cog_commands(cog)
+            all_cmds += cmds
+
         if len(args) == 0:
             send.title = "Lilac Help"
             send.description = "To get the commands in each category, use ```l!help <category-name>```"
-            cats = [cog for cog in self.bot.cogs]
-            cats.sort()
             for cat in cats:
                 cat_cmds = self.bot.get_cog_commands(cat)
                 send.add_field(name=cat, value='{} commands'.format(str(len(cat_cmds))), \
                                 inline=True)
-                
 
+        elif len(args) == 1:
+            search_term = args[0]
+            for cat in cats:
+                if cat.lower() == search_term.lower():
+                    cat_cmds = self.bot.get_cog_commands(cat)
+                    send.title = '{} Commands'.format(cat)
+                    send.description = 'To get more details about each command, use ' +\
+                                     '```l!help <command-name>```'
+                    for cmd in cat_cmds:
+                        send.add_field(name=cmd.name, value=cmd.brief, inline=True)
+                    
+        
         await ctx.send(embed=send)
                 
 
