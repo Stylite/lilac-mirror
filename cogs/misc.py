33e489f86cbb9b6e164b49bf5bfeb017fa0050bf
adx59 <adamxu59@gmail.com>
2018-01-24 17:50:49 -0800

    Add help command

--- cogs/misc.py
+++ cogs/misc.py
@@ -7,6 +7,15 @@ class Misc:
     def __init__(self, bot):
         self.bot = bot
 
+    def usage(self, cmd: commands.command):
+        params_string = ''
+        params = list(cmd.clean_params.items())
+        for param in params:
+            params_string += '<{}>'.format(param[0])
+        
+        return f'{self.bot.command_prefix}{cmd.name} {params_string}' 
+
+
     @commands.command()
     async def help(self, ctx, *args):
         send = discord.Embed()
@@ -27,16 +36,45 @@ class Misc:
                                 inline=True)
 
         elif len(args) == 1:
+            found = [False, False]
             search_term = args[0]
             for cat in cats:
                 if cat.lower() == search_term.lower():
+                    found[0] = True
                     cat_cmds = self.bot.get_cog_commands(cat)
+
                     send.title = '{} Commands'.format(cat)
                     send.description = 'To get more details about each command, use ' +\
                                      '```l!help <command-name>```'
+
                     for cmd in cat_cmds:
+                        if cmd.brief is None and cmd.help is not None:
+                            cmd.brief = cmd.help.split('\n')[0]
+                        if cmd.help is None:
+                            cmd.brief = 'No help message found.'
                         send.add_field(name=cmd.name, value=cmd.brief, inline=True)
+                    break
+            
+            for cmd in all_cmds:
+                if cmd.name.lower() == search_term.lower():
+                    found[1] = True
                     
+                    send.title = 'Help for command `{}`'.format(cmd.name)
+
+                    cmd_help = cmd.help
+                    if not cmd.help:
+                        cmd_help = 'No help message for this command...'
+
+                    send.description = 'Usage: {}\n\n{}'.format(self.usage(cmd), cmd_help)
+                    break
+
+            if found == [False, False]:
+                ctx.send('Sorry, but I couldn\'t find that category/command!')
+                return
+        
+        else:
+            ctx.send(':warning: Too many arguments.')
+            return
         
         await ctx.send(embed=send)
                 
