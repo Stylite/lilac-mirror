88afcdac4a147381cbbbcab6ae23d1f15e46ba00
adx59 <adamxu59@gmail.com>
2018-02-03 17:58:28 -0800

    pls fix

--- cogs/util/devnotif.py
+++ cogs/util/devnotif.py
@@ -55,7 +55,7 @@ class DevNotif:
             embed.colour = 0x2fef00
 
         embed.title = 'Notification (Thing Occured!):'
-        embed.author = f'{self.user.name}#{self.user.discrim} [{self.user.id}]'
+        embed.author = f'{self.user.name}#{self.user.discriminator} [{self.user.id}]'
 
         embed.add_field(name='Type:', value=self.notif_type)
         embed.add_field(name='In Guild:', value=self.guild.name)
